9a09bebb0abb106bb75a15ad2a730915bf24a884
schelihaeva <146697334+schelihaeva@users.noreply.github.com>
2024-09-25 08:12:31 +0300

    Update Cat.java

--- src/Cat.java
+++ src/Cat.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+public class Cat extends Animal{
+    String color;
 
-public class Cat {
+    public Cat(String name, int age, String color) {
+        super(name, age);
+        this.color = color;
+    }
+
+    public void makeSound() {
+        System.out.println("Мяу-мяу!");
+
+    }
+
+    public void purr(){
+        System.out.println("Муррр");
+    }
+
 }
