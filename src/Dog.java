f1f8f8a34ef7b39171b2e6736903ff5ac79f7f6f
schelihaeva <146697334+schelihaeva@users.noreply.github.com>
2024-09-25 08:11:25 +0300

    Update Dog.java

--- src/Dog.java
+++ src/Dog.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+public class Dog extends Animal{
+    String breed;
 
-public class Dog {
+    public Dog(String name, int age, String breed) {
+        super(name, age);
+        this.breed = breed;
+    }
+
+    public void makeSound() {
+        System.out.println("Гав-гав!");
+    }
+
+    public void sit(){  // команда "Сидеть!"
+        System.out.println("Собака выполнила команду-села");
+    }
+
 }
