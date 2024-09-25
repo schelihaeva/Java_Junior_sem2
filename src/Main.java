f0faa4430691fd1a955ae24bfd0cbeb1bd0990ff
schelihaeva <146697334+schelihaeva@users.noreply.github.com>
2024-09-25 08:11:55 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,30 @@
-public class Dog extends Animal{
-    String breed;
+import java.lang.reflect.Method;
 
-    public Dog(String name, int age, String breed) {
-        super(name, age);
-        this.breed = breed;
+public class Main {
+    public static void main(String[] args) {
+
+        Animal[] animals = new Animal[]{
+                new Dog("Тузик", 4, "Питбуль"),
+                new Dog("Грязнуля", 1, "Такса"),
+                new Cat("Васька", 6, "Черно-белый"),
+                new Cat("Рыжик", 3, "Рыжий")
+        };
+
+        for (Animal animal : animals) {
+            System.out.println(animal.getClass().getSimpleName() + ":");
+            System.out.println("Кличка: " + animal.name);
+            System.out.println("Возраст: " + animal.age);
+
+            try {
+                Method method = animal.getClass().getMethod("makeSound");
+                method.invoke(animal);
+            } catch (Exception e) {
+                System.out.println("Метод 'makeSound()' не найден.");
+            }
+
+            System.out.println();
+        }
+
+
     }
-
-    public void makeSound() {
-        System.out.println("Гав-гав!");
-    }
-
-    public void sit(){  // команда "Сидеть!"
-        System.out.println("Собака выполнила команду-села");
-    }
-
 }
